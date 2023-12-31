ed0a9daeae6b89ba8ef353ba554e57661282537e
Alex Haible <52239823+AlexHaible@users.noreply.github.com>
2023-12-31 11:56:12 +0100

    Slowly getting there...

--- main.lua
+++ main.lua
@@ -10,106 +10,102 @@ local L = addon:GetModule('Localization')
 
 -- Get the player's class
 local className, classFilename, classId = UnitClass("player")
--- End of Variables --
 
+local HEAD = "INVTYPE_HEAD"
+local SHOULDER = "INVTYPE_SHOULDER"
+local BODY = "INVTYPE_BODY"
+local CHEST = "INVTYPE_CHEST"
+local ROBE = "INVTYPE_ROBE"
+local WAIST = "INVTYPE_WAIST"
+local LEGS = "INVTYPE_LEGS"
+local FEET = "INVTYPE_FEET"
+local WRIST = "INVTYPE_WRIST"
+local HAND = "INVTYPE_HAND"
+local CLOAK = "INVTYPE_CLOAK"
+local WEAPON = "INVTYPE_WEAPON"
+local SHIELD = "INVTYPE_SHIELD"
+local WEAPON_2HAND = "INVTYPE_2HWEAPON"
+local WEAPON_MAIN_HAND = "INVTYPE_WEAPONMAINHAND"
+local RANGED = "INVTYPE_RANGED"
+local RANGED_RIGHT = "INVTYPE_RANGEDRIGHT"
+local WEAPON_OFF_HAND = "INVTYPE_WEAPONOFFHAND"
+local HOLDABLE = "INVTYPE_HOLDABLE"
+local TABARD = "INVTYPE_TABARD"
+local BAG = "INVTYPE_BAG"
+local PROFESSION_TOOL = "INVTYPE_PROFESSION_TOOL"
+local RING = "INVTYPE_FINGER"
+local TRINKET = "INVTYPE_TRINKET"
+local NECK = "INVTYPE_NECK"
 
--- Debugging --
-categories:AddItemToCategory(54441, L:G("Debugging"))
--- End of Debugging --
+local MISC = "MISCELLANEOUS"
+local CLOTH = "CLOTH"
+local LEATHER = "LEATHER"
+local MAIL = "MAIL"
+local PLATE = "PLATE"
+local COSMETIC = "COSMETIC"
 
--- Functions --
--- Function to check if the item is transmoggable
-local function IsItemTransmoggable(data)
-    if not data.itemInfo or not data.itemInfo.itemID then
-        return false
-    end
+local classArmorTypeMap = {
+    ["DEATHKNIGHT"] = PLATE,
+    ["DEMONHUNTER"] = LEATHER,
+    ["DRUID"] = LEATHER,
+    ["EVOKER"] = MAIL,
+    ["HUNTER"] = MAIL,
+    ["MAGE"] = CLOTH,
+    ["MONK"] = LEATHER,
+    ["PALADIN"] = PLATE,
+    ["PRIEST"] = CLOTH,
+    ["ROGUE"] = LEATHER,
+    ["SHAMAN"] = MAIL,
+    ["WARLOCK"] = CLOTH,
+    ["WARRIOR"] = PLATE,
+}
 
-    -- Use the C_Transmog.CanTransmogItem API to check if the item can be transmogged.
-    local canBeTransmogged = C_Transmog.CanTransmogItem(data.itemInfo.itemID)
-
-    return canBeTransmogged
+function isKnown(itemID)
+    return C_TransmogCollection.PlayerHasTransmog(itemID)
 end
 
--- Function to check if the item is for the player's class
-local function IsItemForMyClass(data)
-    if not data.itemInfo or not data.itemInfo.itemLink then
-        return false
-    end
-
-    -- Get the sourceID from the item link
-    local sourceID = select(2, C_TransmogCollection.GetItemInfo(data.itemInfo.itemLink))
-
-    -- Check if sourceID is valid
-    if not sourceID then
-        return false
-    end
-
-    -- Check if the player can collect this item's appearance
-    local hasItemData, canCollect = C_TransmogCollection.PlayerCanCollectSource(sourceID)
-    return canCollect
-end
-
--- Function to check if the item's appearance is unknown
-local function IsAppearanceUnknown(data)
-    if not data.itemInfo or not data.itemInfo.itemLink then
-        return false
-    end
-
-    -- Get the sourceID from the item link
-    local sourceID = select(2, C_TransmogCollection.GetItemInfo(data.itemInfo.itemLink))
-
-    -- Check if the player has the transmog for this item
-    local hasTransmog = C_TransmogCollection.PlayerHasTransmog(sourceID)
-
-    -- Return true if the appearance is unknown, false otherwise
-    return not hasTransmog
-end
-
--- Function to generate category name for items suitable for the player's class
-local function GetMyClassCategoryName()
-    print("Player's Class: " .. className)
-    return L:G(className .. " Items")
-end
-
--- Function to check if the item is equippable
-local function IsItemEquippable(data)
-    -- Check if itemEquipLoc is present and non-empty
-    if data.itemInfo and data.itemInfo.itemEquipLoc and data.itemInfo.itemEquipLoc ~= "" then
+function isUsableByCurrentClass(data)
+    local itemType = string.upper(data.itemInfo.itemSubType)
+    if classArmorTypeMap[classFilename] == itemType or itemType == COSMETIC or itemType == MISC then
         return true
     end
     return false
 end
--- End of Functions --
 
--- Category Functions --
--- First Category: Suitable for my class and appearance unknown
-categories:RegisterCategoryFunction("ItemsForMyClass", function (data)
-    -- Output to console the item data
-    print("Item Data from IFMC: " .. data.itemInfo.itemLink .. " " .. data.itemInfo.itemEquipLoc)
+local function printTable(tbl, indent)
+    if not indent then indent = 0 end
+    for k, v in pairs(tbl) do
+        formatting = string.rep("  ", indent) .. k .. ": "
+        if type(v) == "table" then
+            print(formatting)
+            printTable(v, indent+1)
+        elseif type(v) == 'boolean' then
+            print(formatting .. tostring(v))      
+        else
+            print(formatting .. v)
+        end
+    end
+end
 
-    if IsItemEquippable(data) and IsItemTransmoggable(data) and IsItemForMyClass(data) and IsAppearanceUnknown(data) then
-        return GetMyClassCategoryName()
+categories:RegisterCategoryFunction("SetAppearanceItemCategories", function (data)
+    if data.itemInfo.itemEquipLoc ~= "" then
+        -- Guard clauses
+        if data.itemInfo.itemEquipLoc == PROFESSION_TOOL or data.itemInfo.itemEquipLoc == BAG or data.itemInfo.itemEquipLoc == RING or data.itemInfo.itemEquipLoc == TRINKET or data.itemInfo.itemEquipLoc == NECK then
+            return nil
+        end
+
+        --printTable(data)
+        
+        if isKnown(data.itemInfo.itemID) then 
+            return L:G("Known Appearances")
+        end
+        -- End of guard clauses
+
+        if isUsableByCurrentClass(data) then
+            return L:G(className .. " Items")
+        else
+            return L:G("Not Usable by " .. className)
+        end
     end
     return nil
 end)
-
--- Second Category: Not suitable for my class but appearance unknown
-categories:RegisterCategoryFunction("ItemsForOtherClasses", function (data)
-    -- Output to console the item data
-    print("Item Data from IFOC: " .. data.itemInfo.itemLink .. " " .. data.itemInfo.itemEquipLoc)
-    if IsItemEquippable(data) and IsItemTransmoggable(data) and not IsItemForMyClass(data) and IsAppearanceUnknown(data) then
-        return L:G("Items for Other Classes")
-    end
-    return nil
-end)
-
--- Third Category: Appearance already known
-categories:RegisterCategoryFunction("SafeForSelling", function (data)
-    -- Output to console the item data
-    print("Item Data from SFS: " .. data.itemInfo.itemLink .. " " .. data.itemInfo.itemEquipLoc)
-    if IsItemEquippable(data) and IsItemTransmoggable(data) and not IsAppearanceUnknown(data) then
-        return L:G("Safe for Selling")
-    end
-    return nil
-end)
--- End of Category Functions --
